390e9e59b811b72feb20cc50b09117ddc81a45fc
Suman Bharati <sumanbharati07@gmail.com>
2024-07-20 15:57:17 -0400

    Styled the Active link in the Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import Link from "next/link";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  // usePathname hook comes with Next JS to get the current path ot the current route.
+  // We are using this hook in this component, and this hook is dependent on the browser APIs we have to
+  //    convert this component to a client component, and we know that we can only access browser APIs in
+  //    client compnent.
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -16,7 +26,12 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 text-xl font-semibold hover:text-zinc-800 transition-colors"
+            className={classnames({
+              'text-sky-600': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'text-xl font-semibold hover:text-zinc-800 transition-colors': true
+            })}
+            // className={`${link.href === currentPath ? 'text-sky-600' : 'text-zinc-500'} text-xl font-semibold hover:text-zinc-800 transition-colors`}
             href={link.href}
           >
             {link.label}
