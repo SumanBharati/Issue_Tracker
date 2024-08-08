42b9632db373324b5d2284d23a8539b76d7559d8
Suman Bharati <sumanbharati07@gmail.com>
2024-08-08 08:55:09 +0530

    Built an API

---
+++ prisma/client.ts
@@ -0,0 +1,18 @@
+// This code here is creating a single instance of Prisma client. 
+// It ensures that we don't have multiple instances running.
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
